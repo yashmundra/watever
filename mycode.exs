d7408288ec764c0d934e9ffb831e1fc7c2689257
yash <yash@claimofund.com>
2019-09-02 23:18:02 -0400

    fd

--- mycode.exs
+++ mycode.exs
@@ -10,7 +10,7 @@ defmodule Vampire do
 end
 
 
-stream = Task.Supervisor.async_stream(MySupervisor,3..10000,Vampire, :printrandom, [], max_concurrency: 6000)
+stream = Task.Supervisor.async_stream(MySupervisor,3..100000,Vampire, :printrandom, [], max_concurrency: 4000)
 
 Enum.to_list(stream)
 
