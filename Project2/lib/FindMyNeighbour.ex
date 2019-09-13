96599291526302244e76f3881d880243608eb399
yash <yash@claimofund.com>
2019-09-13 14:42:34 -0400

    Converted enum to map

--- Project2/lib/FindMyNeighbour.ex
+++ Project2/lib/FindMyNeighbour.ex
@@ -1,28 +1,40 @@
 defmodule FindMyNeighbour do
 
-  def full(id) do
-	#returns the neighbouring ids to send msg to 
+  #pid_map is map having id:pid
+  #ids start from 1
+
+  def full(pid_map,myid) do
+	#returns the neighbouring pids to send msg to  
+  	Map.values(Map.delete(pid_map,myid))
   end
 
-  def line(id) do
-	#returns the neighbouring ids to send msg to 
+  def line(pid_map,id) do
+	#returns the neighbouring pids to send msg to 
+        cond do
+        
+	id==1 -> Map.fetch(pid_map,2) 
+        id==map_size(pid_map) -> Map.fetch(pid_map,id-1) 
+	true -> [Map.fetch(pid_map,id-1),Map.fetch(pid_map,id+1)]
+
+	end      
   end
 
-  def rand2D(positions,myposition,id) do
-	#SPECIAL since it takes positions of other and self
-	#returns the neighbouring ids to send msg to 
+  def rand2D(positions,pid_map,myid) do
+	#SPECIAL since it takes positions of other and self positions is mapping of id to position {x,y}
+	#returns the neighbouring pids to send msg to 
   end
 
-  def 3Dtorus(id) do
-	#returns the neighbouring ids to send msg to 
+  def 3Dtorus(pid_map,id) do
+	#returns the neighbouring pids to send msg to 
+ 
   end
 
-  def honeycomb(id) do
-	#returns the neighbouring ids to send msg to 
+  def honeycomb(pid_map,id) do
+	#returns the neighbouring pids to send msg to 
   end
 
-  def randhoneycomb(id) do
-	#returns the neighbouring ids to send msg to 
+  def randhoneycomb(pid_map,id) do
+	#returns the neighbouring pids to send msg to 
   end
 
 end
