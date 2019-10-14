66fd9f0f47f99765a12e2ec67700e798cddf94be
yash <yash@claimofund.com>
2019-10-14 17:42:46 -0400

    Some more changes. Nothing figures out yet

--- Project_3/lib/RealNode.ex
+++ Project_3/lib/RealNode.ex
@@ -39,29 +39,50 @@ defmodule RealNode do
 
     #SERVER API
     #routing table will be a keyword map with level values mapping to enums with nil in slots where no entry and {nodeid,pid} otherwise
-    #message to node map will store node as key and list of messages that node has
+    #message to node map will store message as key and list of nodes that have that objects data
+    #root node for an object needs to be figured out beforehand in a determininstic fashion
 
-    def handle_cast({:publishObj,msg}, {node_to_message_map, routing_table, node_id}) do
-      
-      {:noreply, {node_to_message_map, routing_table, node_id}}
+    ##################################### PUBLISH ####################################################
+
+    def handle_cast({:publishObj,msg}, {message_to_nodes_map, routing_table, node_id}) do
+      root_pid = route_to_root(msg,routing_table,node_id) 
+      GenServer.cast(root_pid,{:storeObjRef,node_id,msg})
+      #store at the root node reference to local node and message
+      {:noreply, {message_to_nodes_map, routing_table, node_id}}
     end
 
-    def handle_cast({:unpublishObj,msg},{node_to_message_map, routing_table, node_id}) do
-      
-      {:noreply, {node_to_message_map, routing_table, node_id}}
+    def handle_cast({:storeObjRef,node_id,msg},{message_to_nodes_map, routing_table, node_id}) do
+      #update node message map
+      {:noreply, {message_to_nodes_map, routing_table, node_id}}
     end
+
+    #################################### UNPUBLISH  #####################################################
+
+    def handle_cast({:unpublishObj,msg},{message_to_nodes_map, routing_table, node_id}) do
+      #route to root
+      root_pid = route_to_root(msg,routing_table,node_id) 
+      GenServer.cast(root_pid,{:delObjRef,node_id,msg})
+      {:noreply, {message_to_nodes_map, routing_table, node_id}}
+    end
+
+    def handle_cast({:delObjRef,node_id,msg},{message_to_nodes_map, routing_table, node_id}) do
+      #update node message map
+      {:noreply, {message_to_nodes_map, routing_table, node_id}}
+    end
+
+    ###################################  ROUTE TO OBJ ########################################################
     
-    def handle_cast({:routeToObj,obj_id},{node_to_message_map, routing_table, node_id}) do
-      
-      {:noreply, {node_to_message_map, routing_table, node_id}}
+    def handle_cast({:routeToObj,obj_id},{message_to_nodes_map, routing_table, node_id}) do
+
+      {:noreply, {message_to_nodes_map, routing_table, node_id}}
     end
 
-    def handle_cast({:routeToNode,node_id,exact},{node_to_message_map, routing_table, node_id}) do
+    def handle_cast({:routeToNode,node_id,exact},{message_to_nodes_map, routing_table, node_id}) do
       
-      {:noreply, {node_to_message_map, routing_table, node_id}}
+      {:noreply, {message_to_nodes_map, routing_table, node_id}}
     end
 
-    def handle_cast({:initialize,n_id},{node_to_message_map, routing_table, node_id}) do
+    def handle_cast({:initialize,n_id},{message_to_nodes_map, routing_table, node_id}) do
 
       #64 routing levels and 16 slots . levels are zero indexed
       routing_row = Enum.map(1..16, fn x-> nil end)
@@ -70,12 +91,12 @@ defmodule RealNode do
       {:noreply, {%{}, my_routing_table, n_id}}
     end
     
-    def handle_cast({:setNbor,list_of_neighbour_pids},{node_to_message_map, routing_table, node_id}) do
+    def handle_cast({:setNbor,list_of_neighbour_pids},{message_to_nodes_map, routing_table, node_id}) do
       #update routing table
       #updates = Enum.each(list_of_neighbour_pids, fn pid -> {MyApp.hashStuff(pid),pid} end)
       #Enum.each(updates, fn {n,p} -> M)
       
-      {:noreply, {node_to_message_map, routing_table, node_id}}
+      {:noreply, {message_to_nodes_map, routing_table, node_id}}
     end
 
 end
